4e84e0b8f5108ed949e45eee04457376dd3f0029
lscatfish <2561925435@qq.com>
2026-03-31 13:13:58 +0800

    用于重载的基础节点和操作

---
+++ core/base.py
@@ -0,0 +1,25 @@
+# -*- encoding:utf-8 -*-
+# 用于重载的基础节点和操作
+
+
+
+
+class Node: ...
+
+
+class add: ...
+
+
+class minus: ...
+
+
+class negative: ...
+
+
+class multiply: ...
+
+
+class matmul: ...
+
+
+class elementwise_pow: ...
