521835c5ec58e647ef1163ed042c06261839892d
lscatfish <2561925435@qq.com>
2026-04-21 18:46:48 +0800

    格式化

--- minist/analysis.py
+++ minist/analysis.py
@@ -1,32 +1,28 @@
-﻿#分析模型
+﻿# 分析模型
 
 
 import os
-from PIL import Image
 import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
-from minist.model import MNIST_ConvAttnNet,_transform,DEVICE,IMG_FOLDER,model,test_dataset
+from minist.model import MNIST_ConvAttnNet, _transform, DEVICE, IMG_FOLDER, model, test_dataset
+
 
 def load_model():
-	kt=torch.load('./minist/md.pth',map_location=DEVICE)
-	print(kt)
-	model.load_state_dict(kt )
-@torch.no_grad
-def eval_meta(img):
-	model.eval()
-	return model(img)
+    kt = torch.load('./minist/md.pth', map_location = DEVICE)
+    print(kt)
+    model.load_state_dict(kt)
 
-	
+
+@torch.no_grad()
+def eval_meta(img):
+    model.eval()
+    return model(img)
+
 
 if __name__ == "__main__":
-	load_model()
-	data=test_dataset[0]
-	print(torch.argmax(F.softmax(eval_meta(data[0].unsqueeze(0)))))
-	print(data[1])
-
-
-
-	print 
-
+    load_model()
+    data = test_dataset[0]
+    print(torch.argmax(F.softmax(eval_meta(data[0].unsqueeze(0)))))
+    print(data[1])
